0de6ec50a41aeaacf05108b36ac7ece4b3e9d7df
Nikita Utkin <nickus.wanderer@gmail.com>
2024-04-26 13:53:46 +0300

    ADD: test all classes

--- tests/test_characters.py
+++ tests/test_characters.py
@@ -1,14 +1,24 @@
-from ..character_create import Fixer, Media, Exec, Rockerboy, Solo
-from ..character_create import Netrunner, Tech, Medtech, Lawmen, Nomad
+import sys
+sys.path.append('../')
+from character_create import *
+# from ..character_create import Fixer, Media, Exec, Rockerboy, Solo
+# from ..character_create import Netrunner, Tech, Medtech, Lawmen, Nomad
 
-import pytest
+# import pytest
 import yaml
 from pathlib import Path
 
 def test_character():
-    tables = yaml.safe_load(Path(__file__).parent.resolve() / 'data/tables.yaml')
+    tables_path = Path(Path(__file__).parent, '../data/tables.yaml').resolve()
+    with open(tables_path) as fo:
+        tables = yaml.safe_load(fo)
+
     for role in tables['roles']:
-        print(role)
+        role_class = globals()[role.capitalize()]
+        name = None
+        char = role_class(name, role, 'male', tables)
+
+        print(char)
 
 if __name__ == '__main__':
     test_character()
